e49b80d049be87df6edf59f006d9872afd2e08c7
vishal-naveen <vishalnaveen.03@gmail.com>
2023-03-01 10:22:50 -0500

    intial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
