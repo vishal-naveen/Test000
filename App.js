339e9871b3cc4798330287409ba6776ff697f3eb
vishal-naveen <vishal.np03@gmail.com>
2023-03-01 10:53:25 -0500

    export

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 
 const App = () => {
-  return(
-    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Hello World</Text>
       <Text>Hello World</Text>
       <Text>Hello World</Text>
     </View>
-  )
-}
+  );
+};
+
+export default App;
