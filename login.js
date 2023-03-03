651363e96836da8404b056774c3e93a9dffff26a
vishal-naveen <vishalnaveen.03@gmail.com>
2023-03-03 04:59:38 -0500

    login page 0.1

---
+++ login.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Login = () => {
+  return (
+    <View sty></View>
+  );
+};
+
+export default Login;
