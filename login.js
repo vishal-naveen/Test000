fb62a36f78558115bfb4c7150c13415189a9cf6c
vishal-naveen <vishalnaveen.03@gmail.com>
2023-03-06 20:48:13 -0500

    pages

--- login.js
+++ login.js
@@ -1,11 +1,18 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 const Login = () => {
   return (
-    <View sty></View>
+    <View>
+      <Text>Hurricane Help</Text>
+      <Button></Button>
+    </View>
   );
 };
+
+const styles = (StyleSheet.create = {
+  
+})
 
 export default Login;
