96175786cbeecc04000d9e438e0efa577979ad73
karandeep singh <karandeepsingh@karandeeps-MacBook-Air.local>
2023-10-25 15:41:46 +0530

    code up

--- src/components/PasswordIn.js
+++ src/components/PasswordIn.js
@@ -1,19 +1,27 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, TextInput, Text} from 'react-native'
+import {View, TextInput, Text, Platform} from 'react-native'
 
 export default function TInput(props) {
     const {placer, onChangeText, color} = props
      return (
        <View
-         style={{
+         style={Platform.OS =="android"? {
            borderWidth: 1,
            borderColor: '#dfd1b8',
            width: '80%',
            marginBottom: 20,
            borderRadius:10,
            paddingLeft:10
-         }}>
+         }:{
+          borderWidth: 1,
+          borderColor: '#dfd1b8',
+          width: '80%',
+          marginBottom: 20,
+          borderRadius:10,
+          paddingLeft:10,
+          height:40,justifyContent:"center"
+        }}>
          <TextInput textAlign='left' placeholderTextColor={color} color={color} onChangeText={onChangeText} placeholder={placer} secureTextEntry={true}/>
        </View>
      );
